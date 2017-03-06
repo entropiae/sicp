1ea98c182463fb67df9f524cac39d9102a1c6859
Riccardo Cirimelli <rcirimelli@gmail.com>
2017-03-06 22:56:23 +0100

    Add simpler version

--- exercises/1_3.scm
+++ exercises/1_3.scm
@@ -4,8 +4,14 @@
 (define (square x) (* x x))
 
 (define (max x y) (>= x y) x y)
+(define (min x y) (<= x y) x y)
 
 (define (sum-larger-squared x y z)
     (cond ((and (>= x y) (>= x z)) (+ (square x) (square (max y z))))
           ((and (>= y x) (>= y z)) (+ (square y) (square (max x z))))
           (else (+ (square z) (square (max x y))))))
+
+(define (sum-larger-squared-better x y z)
+    (+
+        (square (max x y))
+        (square (max (min x y) z))))
