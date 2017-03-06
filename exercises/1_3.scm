b0d07f3ec142506f71bcb71e889b3a07cbfb7083
Riccardo Cirimelli <rcirimelli@gmail.com>
2017-03-06 22:55:51 +0100

    more lisp-y procedure name

--- exercises/1_3.scm
+++ exercises/1_3.scm
@@ -5,7 +5,7 @@
 
 (define (max x y) (>= x y) x y)
 
-(define (sumlargersquares x y z)
+(define (sum-larger-squared x y z)
     (cond ((and (>= x y) (>= x z)) (+ (square x) (square (max y z))))
           ((and (>= y x) (>= y z)) (+ (square y) (square (max x z))))
           (else (+ (square z) (square (max x y))))))
